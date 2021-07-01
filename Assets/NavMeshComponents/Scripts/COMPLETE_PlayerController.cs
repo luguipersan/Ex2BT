3db391a65eb334d0dbbc259f83c604693038c5d7
luguipersan <72970615+luguipersan@users.noreply.github.com>
2021-07-01 15:36:13 +0100

    porta

--- Assets/NavMeshComponents/Scripts/COMPLETE_PlayerController.cs
+++ Assets/NavMeshComponents/Scripts/COMPLETE_PlayerController.cs
@@ -6,8 +6,12 @@ public class COMPLETE_PlayerController : MonoBehaviour
     [SerializeField]private Camera cam;
     [SerializeField]private NavMeshAgent agent;
 
+    Vector3 mousePos;
+    public Rigidbody rb;
+
     void Update()
     {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -18,5 +22,13 @@ public class COMPLETE_PlayerController : MonoBehaviour
                 agent.SetDestination(hit.point);
             }
         }
+     
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
